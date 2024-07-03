c89c5447c0c9626d30e79902a2bf50bd567e9320
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2024-07-03 15:06:19 -0700

    this section is covering NESTING, CONCAT, SPREAD, FLAT,AND STATIC METTHODS ON ARRAY OR OBJECTS

--- worksheet.js
+++ worksheet.js
@@ -91,3 +91,58 @@ console.log(y, arr2);
 const arr3 = [34, 55, 95, 20, 15, 1, 2, 3, 4, 5, 6, 7];
 c = arr3.splice(4, arr3.length - 2).reverse().toString().charAt(0).toUpperCase(0); 
 console.log(arr3, c);
+
+//                                          #### NESTING, CONCAT & SPREAD OPERATOR
+
+const fruit = ['apple', 'orange', 'pear'];
+const berries = ['strawberry', 'blueberry', 'rasberry'];
+
+fruit.push(berries);
+d = fruit;
+
+//to access the nested array
+d = fruit[3];
+// display rasberry from the nested 
+d = fruit[3][2];
+
+//more example of arrays in another array
+
+const allfruits = [fruit, berries];
+// display rasberry
+d = allfruits[1][2];
+console.log(d);
+
+// CONTACT = taking items from one array and add to items in another array
+
+e = fruit.concat(berries); //displaying all the items inside fruit and berries together
+console.log(e);
+
+// SPREAD OPERATOR (...) = another method of concating used with array or object
+const fruit1 = ['apple', 'orange', 'pear'];
+const berries1 = ['strawberry', 'blueberry', 'rasberry'];
+
+f = [...fruit1, ...berries1];
+console.log(f);
+
+//FLATTEN METHOD = spread all items in an array and its nested arrays 
+
+const g1 = [1, 2, [3, 4], [5, 6,]]; 
+g = g1.flat();
+console.log(g);
+
+
+// STATIC METHODS ON ARRAY OBJECTS
+h = Array.isArray(fruit1);// true
+h = Array.isArray('Hello'); //false
+console.log(h);
+
+i = Array.from('12345');// used for taking html elements
+console.log(i);
+
+// convert the items of variable(s) into an array
+
+const a = 1;
+const af = 2;
+const afr = 3;
+g = Array.of(a, af, afr);
+console.log(g);
