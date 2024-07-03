ea04e9049f1da07d6e973e830e5bac752755b0e8
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2024-07-03 09:21:58 -0700

    This is covering the Javascript array methodes and how to chain methods

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,5 @@
+//                                          ####ARRAY BASIC
+
 let x;
 
 //ARRAY LITERAL
@@ -35,7 +37,57 @@ fruits[fruits.length] = 'banana';
 // replace the last value of an array items with a new value (no method approach)
 fruits[fruits.length -1] = 'mango'
 
-
-
-
 console.log(x);
+
+
+
+//                                              ####ARRAY METHODS
+
+//         THE FOLLOWING ARRAY METHODS ARE USED FOR MANIPULATING AN ARRAY
+
+const arr = [34, 55, 95, 20, 15];
+
+//ADD value to the END of an array items
+arr.push(100)
+
+// REMOVE value from the END of an array items
+arr.pop();
+
+//ADD value to the BEGINNING of an array items
+arr.unshift(99);
+
+//REMOVE value from the BEGINNING of an array items
+arr.shift();
+
+// reverse the array
+arr.reverse();
+
+
+console.log(arr);
+
+
+//          THE FOLLOWING ARRAY METHODS ARE USED FOR GETTING VALUES FROM AN ARRAY
+let y;
+const arr1 = [34, 55, 95, 20, 15];
+
+// check if a values exist within an array
+y = arr1.includes(55);
+
+//check the index position of the value within an array
+y = arr1.indexOf(34);
+
+// only displays or make a copy of specific value in an array giving a specified starting and ending position without changing values of the array
+y =arr1.slice(2,4)
+
+console.log(arr1);
+console.log(y, arr1);
+
+// splice can be used to remove a single value from an array e.g (remove 95)
+const arr2 = [34, 55, 95, 20, 15];
+y = arr2.splice(2, 1); // taking removing only 95 from the array
+console.log(y, arr2);
+
+// how to chain methods
+const arr3 = [34, 55, 95, 20, 15, 1, 2, 3, 4, 5, 6, 7];
+c = arr3.splice(4, arr3.length - 2).reverse().toString().charAt(0).toUpperCase(0); 
+console.log(arr3, c);
