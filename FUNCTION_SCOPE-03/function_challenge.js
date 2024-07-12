e610efe821c38ad8b4dfd83da0516546a0c2bc5f
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2024-07-12 13:46:49 -0700

    Creating an arrow function called minMax () that takes in an array of numbers and returns an object with the minimum and maximum numbers in the array.

--- FUNCTION_SCOPE-03/function_challenge.js
+++ FUNCTION_SCOPE-03/function_challenge.js
@@ -28,3 +28,45 @@ getCelsius(100);
 const getCelsius2 = F => ((F - 32) * 5) / 9;
 
 console.log(getCelsius2(32));
+
+// SOLUTION IN ONE LINE ARROW FUNCTION
+
+const getCelsius3 = F => console.log((F - 32) * 5 / 9);
+
+getCelsius3(100);
+
+// SOLUTION IN TEMPLATE LITERAL
+
+const getCelsius4 = F => console.log(`The temperature is ${((F - 32) * 5) / 9} \xB0C`);
+
+getCelsius4(32);
+
+// SOLUTION IN STRING CONCATENATION
+
+const getCelsius5 = F => console.log('The temperature is ' + ((F - 32) * 5 / 9) + ' \xB0C');
+
+getCelsius5(32);
+
+
+
+
+/*
+2. Creat an arrow function called minMax () that takes in an array of numbers and returns an object with the minimum and maximum numbers in the array.
+Expected Result:
+console. log (minMak( [1, 2, 3, 4, 51));
+// { min: 1, max: 5 }
+Hints:
+1. You can use Math.min() and Math.max() to get the min and max of a list of numbers.
+2. You can use the Spread ...operator to spread the values of an array into separate arguments.
+*/
+
+// SOLUTION IN REGULAR FUNCTION
+
+function minMax(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    return { min, max };
+}
+ console.log(minMax([1, 2, 3, 4, 51]));
+
+// SOLUTION IN ARROW FUNCTION
